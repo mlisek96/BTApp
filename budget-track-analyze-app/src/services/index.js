85b02e0568e7c874470adcfbe606c529fe595c2d
mlisek96 <96mateuszlis@gmail.com>
2022-10-25 14:18:09 +0200

    Created localStorage logic while adding name

---
+++ budget-track-analyze-app/src/services/index.js
@@ -0,0 +1,1 @@
+import './localStorage/localStorage.LogIn.js'
--- budget-track-analyze-app/src/views/LogIn/LogIn.jsx
+++ budget-track-analyze-app/src/views/LogIn/LogIn.jsx
@@ -1,10 +1,48 @@
-import {Link} from "react-router-dom"
+import {useState} from "react";
 import {TextInput, Paper, Title, Text, Container, Button, Image} from '@mantine/core';
 import './LogIn.scss'
 import {PageFooterBeforeMainPage} from "../../components/PageFooterBeforeMainPage/PageFooterBeforeMainPage";
 import image from '../../images/picture-background-2.png'
 
+const inputName = document.getElementById('inputName');
+
+const validateFunction = name => {
+    if (!name) {
+        return 'Entering name is required'
+    } else if (name.length <= 2) {
+        return 'The name must be more than two characters long'
+    } else {
+        return null
+    }
+}
+
 export function LogIn() {
+    const [errorMsg, setErrorMsg] = useState(null)
+    const [name, setName] = useState('')
+
+    function handleChange(event) {
+        setName(event.target.value)
+    }
+
+    function handleClick(event) {
+        event.preventDefault();
+        const errorMsg = validateFunction(name);
+
+        if(errorMsg) {
+            setErrorMsg(errorMsg)
+            console.log('błąd - name')
+            return
+        }
+
+        console.log('submitted', name)
+        localStorage.setItem('valueNameTaken', name);
+        location.href = '/main-page';
+    }
+
+    if (localStorage.getItem('valueNameTaken')) {
+        location.href = '/main-page';
+    }
+
     return (
         <div className='LogIn'>
             <Container className='LogIn-container'>
@@ -17,8 +55,16 @@ export function LogIn() {
                     <Text color="dimmed" size="sm" align="center" mt={5}>
                         Give us your name and we will organize our application for you :)
                     </Text>
-                    <TextInput label="Name" placeholder="Enter your name here" required/>
-                    <Button className='LogIn-container__content-btn' component={Link} to='/main-page' fullWidth mt="xl">
+
+                    {errorMsg && (<span className='LogIn-container__content-errorMsg'>{errorMsg}</span>)}
+
+                    <TextInput id='inputName' onChange={handleChange} label="Name" placeholder="Enter your name here" required/>
+                    <Button
+                        onClick={handleClick}
+                        className='LogIn-container__content-btn'
+                        fullWidth
+                        mt="xl"
+                    >
                         Sign in
                     </Button>
                 </Paper>
