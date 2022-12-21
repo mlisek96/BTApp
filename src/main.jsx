298815500cf57f6403f8382c5907dddc9548a0d8
mlisek96 <96mateuszlis@gmail.com>
2022-12-21 14:57:26 +0100

    update route path to fix deploy

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import {HashRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <HashRouter>
-          <App />
-      </HashRouter>
+    <App />
   </React.StrictMode>
 )
