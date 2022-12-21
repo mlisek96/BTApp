c8b9be25f6f33caa4b3cf339b0d031a214f9203e
mlisek96 <96mateuszlis@gmail.com>
2022-12-21 14:47:09 +0100

    update route path to fix deploy

--- src/App.jsx
+++ src/App.jsx
@@ -23,11 +23,11 @@ function App() {
                 <Routes>
                     <Route path='/BTApp' element={<HeroPage/>}/>
                     <Route path='/login' element={<LogIn/>}/>
-                    <Route path='/layout' element={<Layout/>}>
-                        <Route path='/layout/main-page' element={<MainPage/>}/>
-                        <Route path='/layout/add-expense' element={<AddExpense/>}/>
-                        <Route path='/layout/expenses-table' element={<ExpensesTable/>}/>
-                        <Route path='/layout/expenses-table/by-category' element={<ExpensesTableCategory/>}/>
+                    <Route path='/' element={<Layout/>}>
+                        <Route path='/main-page' element={<MainPage/>}/>
+                        <Route path='/add-expense' element={<AddExpense/>}/>
+                        <Route path='/expenses-table' element={<ExpensesTable/>}/>
+                        <Route path='/expenses-table/by-category' element={<ExpensesTableCategory/>}/>
                     </Route>
                     <Route path='*' element={<NotFoundPage/>}/>
                 </Routes>
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import {HashRouter} from "react-router-dom";
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.scss'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+      <HashRouter>
+          <App />
+      </HashRouter>
   </React.StrictMode>
 )
