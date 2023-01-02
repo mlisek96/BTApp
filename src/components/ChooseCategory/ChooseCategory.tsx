ba70a755ea28154730a8afde56745cccd1b144db
mlisek96 <96mateuszlis@gmail.com>
2023-01-02 17:57:32 +0100

    Update icon's path to deploy

--- src/components/ChooseCategory/ChooseCategory.tsx
+++ src/components/ChooseCategory/ChooseCategory.tsx
@@ -4,56 +4,56 @@ import './ChooseCategory.scss'
 
 const data = [
     {
-        image: '../src/images/icon-entertainment.png',
+        image: '../../images/icon-entertainment.png',
         label: 'Entertainment',
         value: 'Entertainment',
         description: 'Like: movie, games, music, sports, others',
     },
 
     {
-        image: '../src/images/icon-groceries.png',
+        image: '../../images/icon-groceries.png',
         label: 'Groceries',
         value: 'Groceries',
         description: 'Just simple daily groceries',
     },
 
     {
-        image: '../src/images/icon-dinning out.png',
+        image: '../../images/icon-dinning out.png',
         label: 'Dinning out',
         value: 'Dinning out',
         description: 'Just simple meeting up with friends',
     },
 
     {
-        image: '../src/images/icon-home.png',
+        image: '../../images/icon-home.png',
         label: 'Home',
         value: 'Home',
         description: 'Like: rent, household supplies, furniture, maintenance, others',
     },
 
     {
-        image: '../src/images/icon-life.png',
+        image: '../../images/icon-life.png',
         label: 'Life',
         value: 'Life',
         description: 'Like: insurance, clothing, medical expenses, taxes, education, childcare, others',
     },
 
     {
-        image: '../src/images/icon-transport.png',
+        image: '../../images/icon-transport.png',
         label: 'Transportation',
         value: 'Transportation',
         description: 'Like: parking, car, bus/train, gas/fuel, plane, taxi, bicycle, hotel, others',
     },
 
     {
-        image: '../src/images/icon-utilities.png',
+        image: '../../images/icon-utilities.png',
         label: 'Utilities',
         value: 'Utilities',
         description: 'Like: electricity, heat/gas, water, TV/phone/Internet, trash, others',
     },
 
     {
-        image: '../src/images/icon-holiday.png',
+        image: '../../images/icon-holiday.png',
         label: 'Holiday',
         value: 'Holiday',
         description: 'Just all your expenses for relax and holiday',
