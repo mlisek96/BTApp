6b86e4ef4ade09e6a3ad35cb23bd83e56ab8a197
Mateusz Lis <110568418+mlisek96@users.noreply.github.com>
2023-09-28 22:48:42 +0200

    update: basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,11 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import {MantineProvider} from '@mantine/core';
-import {Layout} from "./components/Layout/Layout";
-import {MainPage} from "./views/MainPage.jsx";
-import {Home} from "./views/HeroPage.jsx";
-import {LogIn} from "./views/LogIn.jsx";
-import {AddExpense} from "./views/AddExpense.jsx";
-import {ExpensesTable} from "./views/ExpensesTable.jsx";
-import {ExpensesTableCategory} from "./views/ExpensesTableCategory.jsx";
-import {NotFoundPage} from "./views/404Page.jsx";
+import { MantineProvider } from '@mantine/core';
+import { RouterProvider } from 'react-router-dom';
+import { router } from '$/router/router';
 import './App.scss';
 
-function App() {
-    return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{
-                fontFamily: 'Poppins, sans serif',
-            }}
-        >
-            <Router>
-                <Routes>
-                    <Route path='/BTApp' element={<Home/>}/>
-                    {/*<Route path='/' element={<Home/>}/>*/}
-                    <Route path='/login' element={<LogIn/>}/>
-                    <Route path='/' element={<Layout/>}>
-                        <Route path='/main-page' element={<MainPage/>}/>
-                        <Route path='/add-expense' element={<AddExpense/>}/>
-                        <Route path='/expenses-table' element={<ExpensesTable/>}/>
-                        <Route path='/expenses-table/by-category' element={<ExpensesTableCategory/>}/>
-                    </Route>
-                    <Route path='*' element={<NotFoundPage/>}/>
-                </Routes>
-            </Router>
-        </MantineProvider>
-    );
-}
-
-export default App
+const App = () => (
+  <MantineProvider theme={{ fontFamily: 'Poppins, sans serif' }}>
+    <RouterProvider router={router} />
+  </MantineProvider>
+);
+export default App;
